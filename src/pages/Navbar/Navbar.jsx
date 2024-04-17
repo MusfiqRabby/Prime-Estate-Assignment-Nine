dda2d021663ebe50c363345ff811ad3a609b1237
musfiqrabby <musfiqrabby69@gmail.com>
2024-04-17 21:53:36 +0600

    tooltip added

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -5,67 +5,58 @@ import { AuthContext } from "../../FirebaseProvider/FirebaseProvider";
 
 const Navbar = () => {
 
-  
-  const {logOut, user} = useContext(AuthContext);
-   
-    const navLinks = <>
-        <li> <NavLink to='/'>Home</NavLink> </li>
-        <li> <NavLink to='/update profile'>Update Profile</NavLink></li>
-        <li> <NavLink to='/contact'>Contact</NavLink> </li>
-    </>
+
+  const { logOut, user } = useContext(AuthContext);
+
+  const navLinks = <>
+    <li> <NavLink to='/'>Home</NavLink> </li>
+    <li> <NavLink to='/update profile'>Update Profile</NavLink></li>
+    <li> <NavLink to='/contact'>Contact</NavLink> </li>
+  </>
 
 
-    return (
+  return (
     <div className="navbar bg-base-100">
-     <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        {navLinks}
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">Prime-Estate</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    {navLinks}
-    </ul>
-  </div>
-  <div className="navbar-end ">
-
-     {
-        user? <div className="dropdown dropdown-end">
-         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-          <img src={user?.photoURL || "https://i.ibb.co/cv8B8ws/19021605.jpg" } />
-         </div>
-        </label>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-           <li>
-         <button className="btn btn-sm btn-ghost  "> 
-          {user?.displayName || 'user not found'}
-         </button>
-         </li>
-         <li>
-         <Link className="text-center items-center btn btn-sm  btn-ghost" to='/update profile'>Update Profile</Link>
-         </li>
-        <li>
-       <button
-             onClick={logOut}
-              className="btn btn-sm  btn-ghost">Logout</button>
-          </li>
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Prime-Estate</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navLinks}
         </ul>
       </div>
-        :
-     <Link to='/login'>
-     <button className="btn btn-sm  btn-ghost">Login</button>
-      </Link>
-     }
-  </div>
-</div>
-    );
+      <div className="navbar-end ">
+
+        {
+          user ?
+            <>
+              <div className="dropdown dropdown-end btn btn-circle tooltip tooltip-primary tooltip-left" data-tip={user?.displayName || 'user name not found'}>
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full ">
+                    <img src={user?.photoURL || "https://i.ibb.co/cv8B8ws/19021605.jpg"} />
+                  </div>
+                </label>
+              </div>
+              <button
+                onClick={logOut}
+                className="btn btn-sm  btn-ghost">Logout</button>
+            </>
+            :
+            <Link to='/login'>
+              <button className="btn btn-sm  btn-ghost">Login</button>
+            </Link>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
